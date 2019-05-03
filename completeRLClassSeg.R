f2d143c5a5eaf6404f46559a98c7f7ba74f89e92
Patrick Sogno <44399629+PatrickSog@users.noreply.github.com>
2019-05-03 12:40:52 +0200

    Create completeRLClassSeg.R
    This is the deposed version of Landklif_MergeSegmentClassify.R
    Please find the current code via this link: https://github.com/PatrickSog/Patrick/blob/Landklif/Landklif_MergeSegmentClassify.R

---
+++ completeRLClassSeg.R
@@ -0,0 +1,2 @@
+#### This code does nothing. The current version of the code has been renamed and can be found on github:
+#### Link: https://github.com/PatrickSog/Patrick/blob/Landklif/Landklif_MergeSegmentClassify.R
